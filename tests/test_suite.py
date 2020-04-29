92e16c3580af8bbd3eb1eaccdf1bf8f84e888e99
Nick Eubank <nick@nickeubank.com>
2020-04-29 09:30:44 -0400

    delete accidentally added junk files

--- tests/test_suite.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-df = gpd.read_file('/users/nick/dropbox/rodden/US_clustering/'
-                   'source_data/precinct_returns/'
-                   'precincts_w_countyid.shp')
-
-
-df = df[df.STATE == "42"]
-df = df[['P2008_D', 'P2008_R', 'geometry']]
