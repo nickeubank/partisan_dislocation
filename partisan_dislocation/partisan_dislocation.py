8f5e75bbb58f42cf168c716b25bd23db38e9d241
Nick Eubank <nick@nickeubank.com>
2020-04-29 09:27:25 -0400

    move most code over to better sketch API #4

--- partisan_dislocation/partisan_dislocation.py
+++ partisan_dislocation/partisan_dislocation.py
@@ -5,10 +5,6 @@ import pandas as pd
 from shapely.geometry import Point
 import random
 
-#precincts = gdp.read_
-
-
-
 def random_points_in_polygon(precincts, k,
                              p=None, post_sampling_k=None,
                              dem_vote_count_column="dem",
@@ -45,7 +41,7 @@ def random_points_in_polygon(precincts, k,
     if p is not None and post_sampling_k is not None:
         raise ValueError("Cannot use both p and post_sampling_k.")
 
-        
+
     gf = pd.DataFrame(columns=['dem','KnnShrDem', 'GEOID10','geometry'])
 
     for index, row in df.iterrows():
@@ -60,14 +56,14 @@ def random_points_in_polygon(precincts, k,
 
         points_to_add =  np.random.binomial(int(row[election+"R"]), prob)
 
-        points = random_points_in_polygon(points_to_add, row.geometry)
+        points = _make_random_points(points_to_add, row.geometry)
 
         for point in points:
             gf=gf.append({'dem': 0, 'KnnShrDem': None,
                           'GEOID10': row["GEOID10"], 'geometry': point}, ignore_index=True)
 
 
-def random_points_in_polygon(number, polygon):
+def _make_random_points(number, polygon):
     #Generates number of uniformly distributed points in polygon
     points = []
     min_x, min_y, max_x, max_y = polygon.bounds
@@ -78,3 +74,51 @@ def random_points_in_polygon(number, polygon):
             points.append(point)
             i += 1
     return points
+
+
+def calculate_voter_knn(voter_points, k, target_column='dem'):
+    """
+        Calculation composition of nearest neigbhors.
+
+        :param voter_points: :class:`geopandas.GeoDataFrame`.
+              GeoDataFrame of voter points
+        :param k: Num nearest neighbors to consider.
+        :param target_column: Feature to average
+    """
+
+    tree = cKDTree(list(zip(voter_points["geometry"].x, voter_points["geometry"].y)))
+
+    dd, ii = tree.query(list(zip(voter_points["geometry"].x, voter_points["geometry"].y)), k=k)
+
+    dd = None
+
+
+    its = 0
+    for index, row in voter_points.iterrows():
+        row[f'KnnShr{target_column}'] = sum(voter_points[target_column][ii[its]])/k
+        its+=1
+
+
+    voter_points[target_column] = pd.to_numeric(voter_points[target_column])
+    voter_points[f"KnnShr{target_column}"] = pd.to_numeric(voter_points[f"KnnShr{target_column}"])
+
+def calculate_dislocation(voter_points, districts,
+                          knn_column = 'KnnShrDem',
+                          district_voteshare='dem'):
+    """
+        Calculation difference between knn dem share
+        and dem share of assigned district
+
+        :param voter_points: :class:`geopandas.GeoDataFrame`.
+              GeoDataFrame of voter points.
+        :param voter_points: :class:`geopandas.GeoDataFrame`.
+              GeoDataFrame of district polygons.
+        :param knn_column: Column of `voter_points` with kNN scores
+        :param district_voteshare: Column of `districts` with
+              district feature to difference from `knn_columns`.
+    """
+
+    districts = districts.to_crs(voter_points.crs)
+    voter_points.overlay(districts[[district_voteshare]]
+    voter_points['dislocation'] = voter_points[knn_column] -
+                                  voter_points[district_voteshare]
