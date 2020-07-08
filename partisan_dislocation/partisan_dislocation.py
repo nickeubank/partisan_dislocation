e4ab8f85d6187b000574ed0199b58960dff8d445
Vivek Sahukar <viveksahukar@gmail.com>
2020-07-08 12:25:35 -0400

    Fixed inconsistencies in all functions where previously the dataframe was converted to and from geodataframe and string manipulations are deleted to make code simpler; now throughout geodataframe is used

--- partisan_dislocation/partisan_dislocation.py
+++ partisan_dislocation/partisan_dislocation.py
@@ -4,12 +4,13 @@ from scipy.spatial import cKDTree
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
+from shapely.geometry import Polygon
 import random
 import os
 
-def get_geodataframe(precinct_file, district_file, data_path="../data"):
+def get_geodataframe(precinct_file, district_file, data_path='../data'):
     "Read the precinct and district files from the defined data path and return precinct and district geodataframes"
-    
+
     # read the geopandas precinct file
     fpath_precinct = os.path.join(data_path, precinct_file)
     precinct_gdf = gpd.read_file(fpath_precinct)
@@ -21,7 +22,7 @@ def get_geodataframe(precinct_file, district_file, data_path="../data"):
     return precinct_gdf, district_gdf
 
 def _make_random_points(number, polygon):
-    #Generates number of uniformly distributed points in polygon
+    "Generates number of uniformly distributed points in polygon"
     points = []
     min_x, min_y, max_x, max_y = polygon.bounds
     i= 0
@@ -52,30 +53,22 @@ def random_points_in_polygon(precincts, p=0.01,
               Random state or seed passed to numpy.
     """
     # Make master dataframe
-    gf = gpd.GeoDataFrame(columns=['Dem','KnnShrDem', 'geometry'])
+    gf = gpd.GeoDataFrame(columns=['Dem', 'KnnShrDem', 'geometry'])
     election = "P2008_"
     for index, row in precincts.iterrows():
         # Loop over dems and republicans
         for party in [dem_vote_count_column, repub_vote_count_column]:
-            points_to_add =  np.random.binomial(int(row[election + party]), p)
+            points_to_add = np.random.binomial(int(row[election + party]), p)
             points = _make_random_points(points_to_add, row.geometry)
             for point in points:
-                if party == "D": 
+                if party == "D":
                     dem_value = 1
-                else: 
+                else:
                     dem_value = 0
 
                 gf = gf.append({'Dem': dem_value, 'KnnShrDem': None, 'geometry': point}, ignore_index=True)
 
-
-    # Do string manipulations to separate out x and y coordinate from point attribute in geometry column
-    gf['geometry'] = gf['geometry'].astype(str)
-    gf['geometry'] = gf['geometry'].str.strip()
-    gf['geometry'] = gf['geometry'].str.replace('POINT \(', '')
-    gf['geometry'] = gf['geometry'].str.replace('\)', '')
-    gf[['x', 'y']] = gf.geometry.str.split(' ', expand=True)
-    gf.drop(columns=['geometry'], inplace=True)
-    gf[['Dem', 'x', 'y']] = gf[['Dem', 'x', 'y']].apply(pd.to_numeric)
+    gf['Dem'] = gf['Dem'].astype(int)
     gf['KnnShrDem'] = gf['KnnShrDem'].astype(None)
 
     return gf
@@ -90,23 +83,16 @@ def calculate_voter_knn(voter_points, k, target_column='Dem'):
         :param target_column: Feature to average
     """
 
-    tree = cKDTree(list(zip(voter_points.x, voter_points.y)))
+    tree = cKDTree(list(zip(voter_points['geometry'].x, voter_points['geometry'].y)))
 
-    dd, ii = tree.query(list(zip(voter_points.x, voter_points.y)), k=k)
+    dd, ii = tree.query(list(zip(voter_points['geometry'].x, voter_points['geometry'].y)), k=k)
 
     its = 0
     for index, row in voter_points.iterrows():
-        row[f'KnnShr{target_column}'] = sum(voter_points[target_column][ii[its]])/k
-        its+=1
-        
-    voter_points[target_column] = pd.to_numeric(voter_points[target_column])
-    voter_points[f"KnnShr{target_column}"] = pd.to_numeric(voter_points[f"KnnShr{target_column}"])
+        voter_points.at[index, f'KnnShr{target_column}'] = sum(voter_points[target_column][ii[its]]) / k
+        its += 1
 
-    voter_points['x'] = voter_points['x'].astype(float)
-    voter_points['y'] = voter_points['y'].astype(float)
-    
-    return gpd.GeoDataFrame(voter_points, geometry=gpd.points_from_xy(voter_points.x, voter_points.y))
-
+    return voter_points
 def calculate_dislocation(voter_points, district, knn_column='KnnShrDem', dem_column='dem'):
     """
         Calculation difference between knn dem share
@@ -118,14 +104,14 @@ def calculate_dislocation(voter_points, district, knn_column='KnnShrDem', dem_co
               GeoDataFrame of district polygons.
         :param knn_column: Column of `voter_points` with kNN scores
     """
-    
+
     # Put both geodataframes in common projection
     districts = district.to_crs(voter_points.crs)
-    
-    # Calculate district dem share
-    dist_voter_point= gpd.sjoin(districts, voter_points, how='inner')
 
-    #Calculate democrat share for each district
+    # Calculate district dem share
+    dist_voter_point = gpd.sjoin(districts, voter_points, how='inner')
+
+    # Calculate democrat share for each district
     dist1 = dist_voter_point.groupby(['NAMELSAD']).agg(district_demshare=pd.NamedAgg(column='Dem', aggfunc=np.mean))
     dist1 = dist1.reset_index()
 
@@ -138,7 +124,7 @@ def calculate_dislocation(voter_points, district, knn_column='KnnShrDem', dem_co
     # Select relevant columns
     dislocation_score_df = final_df[['NAMELSAD', 'Dem', 'KnnShrDem', 'district_demshare', 'dislocation', 'geometry']]
 
-    # final dataframe with dislocation score calculated for each voter    
+    # final dataframe with dislocation score calculated for each voter
     return dislocation_score_df
 
 # Uncomment following code to test all functions with NC state
